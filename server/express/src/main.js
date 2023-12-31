5e7e9b0c120d7eef3e6ad0a58b6a0d5372f27a6f
Vincent Valdez <vmdvaldez@hotmail.com>
2023-12-31 07:44:50 -0500

    add cors package

--- server/express/src/main.js
+++ server/express/src/main.js
@@ -1,7 +1,19 @@
+const cors = require('cors');
+
 const express = require('express');
 const app = express()
 
 const pool = require('./pgdb');
+
+app.use(cors());
+
+app.get('/', async (req,res)=>{
+
+    const q = await pool.query('SELECT * FROM activity');
+
+    res.send(q.rows);
+})
+
 
 const port = 3000
 app.listen(port, ()=>{
