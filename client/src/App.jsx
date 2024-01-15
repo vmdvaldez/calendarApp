b3453d5333e4abf2d8411a3be2bea19888a30e9d
Vincent Valdez <vmdvaldez@hotmail.com>
2024-01-15 02:46:29 -0500

    Fix Activity List state to include activity id, and passed onto Outletcontext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,67 @@
 import styles from './App.module.css';
-import Calendar from './components/Calendar/Calendar'
 import NavBar from './components/NavBar';
 
 import { Outlet } from 'react-router';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [activityList, setActivityList] = useState([]);
+  const [categoryList, setCategoryList] = useState([]);
+
+  useEffect(()=>{
+    async function getActivities(){
+        const protocol = "http://"
+        const url = import.meta.env.VITE_BACKEND_URL
+        const port = import.meta.env.VITE_BACKEND_PORT
+        const res = await fetch(`${protocol}${url}:${port}/activity`, {
+            method: "GET",
+            headers:{
+                mode: "cors"
+            }
+        })
+        const json =  await res.json();
+
+        return json
+    }
+
+    getActivities().then(activities => {
+      setActivityList(activities.map(activity=>({
+        id: activity.uid,
+        name: activity.name    
+      })));
+    });
+
+},[])
+
+  useEffect(()=>{
+      async function getCategories(){
+          const protocol = "http://"
+          const url = import.meta.env.VITE_BACKEND_URL
+          const port = import.meta.env.VITE_BACKEND_PORT
+          const res = await fetch(`${protocol}${url}:${port}/categories`, {
+              method: "GET",
+              headers:{
+                  mode: "cors"
+              }
+          })
+          const json =  await res.json();
+          return json
+      }
+
+      getCategories().then(categories => {
+          setCategoryList(categories.map(category=>category.name));
+      });
+  },[]);
+
+  console.log(activityList);
 
   return (
     <div className={styles.appcontainer}>
       <NavBar />
-      <Outlet />
+      <Outlet context={{
+        activityList, setActivityList,
+        categoryList, setCategoryList
+        }}/>
     </div>
   )
 }
